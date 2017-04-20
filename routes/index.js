50bedf3869b1f2e967cf85099239c5aef5df14f7
Regis Millet <regmillet@ct2c.fr>
2017-04-20 10:38:39 +0200

    Initial commit

---
+++ routes/index.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { 
+    title: 'Todo List', 
+    todos: [{
+      description: {
+        'fr': 'une tache',
+        'en': 'a task'
+      },
+      id: '1'
+    }]
+  });
+});
+
+module.exports = router;
